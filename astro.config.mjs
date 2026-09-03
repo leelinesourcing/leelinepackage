bdb487610d5c06d361eae755958f8b5e28cf64e5
leelinesourcing <leelinesourcing@gmail.com>
2026-09-03 15:07:04 +0800

    首页

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'static',
+  image: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'pub-f65f4d4fa9664133a4ceca1f5d43e24d.r2.dev' },
+    ],
+  },
   adapter: cloudflare({
     imageService: 'compile',
   }),
